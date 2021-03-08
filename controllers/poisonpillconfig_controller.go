5ee38f963f5e88c4715f46d67a31a8243899f65a
Nir <niry@redhat.com>
2021-03-08 10:08:53 +0200

    Add RBAC rules required to run the poison pill operator
    Signed-off-by: Nir <niry@redhat.com>

--- controllers/poisonpillconfig_controller.go
+++ controllers/poisonpillconfig_controller.go
@@ -19,6 +19,10 @@ package controllers
 import (
 	"context"
 	"io/ioutil"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"log"
+	"regexp"
+	"strings"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -38,33 +42,62 @@ type PoisonPillConfigReconciler struct {
 
 // +kubebuilder:rbac:groups=config.poison-pill.io.poison-pill.io,resources=poisonpillconfigs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=config.poison-pill.io.poison-pill.io,resources=poisonpillconfigs/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups="",resources=daemonsets,verbs=get;update;patch;create
-// +kubebuilder:rbac:groups="apps",resources=daemonsets,verbs=get;update;patch;create
+// +kubebuilder:rbac:groups=machine.openshift.io,resources=machines,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=machine.openshift.io,resources=machines/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups="",resources=nodes,verbs=get;list;watch;update;delete;create
+// +kubebuilder:rbac:groups="",resources=daemonsets,verbs=get;list;watch;update;patch;create
+// +kubebuilder:rbac:groups="apps",resources=daemonsets,verbs=get;list;watch;update;patch;create
+// +kubebuilder:rbac:groups="extensions",resources=daemonsets,verbs=get;list;watch;update;patch;create
+// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=roles,verbs=get;update;patch;create;escalate
+// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=rolebindings,verbs=get;update;patch;create;escalate
+// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=clusterroles,verbs=get;update;patch;create;escalate
+// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=clusterrolebindings,verbs=get;update;patch;create;escalate
+// +kubebuilder:rbac:groups="",resources=services,verbs=get;update;patch;create
+// +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;update;patch;create
+// +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;update;patch;create
+// +kubebuilder:rbac:groups="security.openshift.io",resources=securitycontextconstraints,verbs=use,resourceNames=privileged
+// +kubebuilder:rbac:groups="config.openshift.io",resources=infrastructures,verbs=get;list;watch
 
 func (r *PoisonPillConfigReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	_ = context.Background()
 	_ = r.Log.WithValues("poisonpillconfig", req.NamespacedName)
 
-	content, err := ioutil.ReadFile("install/poison-pill-deamonset.yaml")
+	content, err := ioutil.ReadFile("install/poison-pill-deamonset-with-rbac.yaml")
 	if err != nil {
 		r.Log.Error(err, "failed to read ds yaml")
 		return ctrl.Result{}, err
 	}
 	r.Log.Info(string(content))
 
-	decode := scheme.Codecs.UniversalDeserializer().Decode
-	ppillDsObj, _, err := decode(content, nil, nil)
-
-	if err != nil {
-		r.Log.Error(err, "failed to decode poison pill daemonset yaml")
-		return ctrl.Result{}, err
+	//decode := scheme.Codecs.UniversalDeserializer().Decode
+	//ppillDsObj, _, err := decode(content, nil, nil)
+	//
+	//if err != nil {
+	//	r.Log.Error(err, "failed to decode poison pill daemonset yaml")
+	//	return ctrl.Result{}, err
+	//}
+	objects := r.parseK8sYaml(content)
+	for _, obj := range objects {
+		err = r.Client.Create(context.Background(), obj)
+		if err != nil {
+			if errors.IsAlreadyExists(err) {
+				err = r.Client.Update(context.Background(), obj)
+				if err != nil {
+					r.Log.Error(err, "failed to update poison pill runtime object", "kind", obj.GetObjectKind().GroupVersionKind().Kind)
+					return ctrl.Result{}, err
+				}
+			} else {
+				r.Log.Error(err, "failed to create poison pill runtime object", "kind", obj.GetObjectKind().GroupVersionKind().Kind)
+				return ctrl.Result{}, err
+			}
+		}
 	}
 
-	err = r.Client.Create(context.Background(), ppillDsObj)
-	if err != nil {
-		r.Log.Error(err, "failed to create poison pill daemonset")
-		return ctrl.Result{}, err
-	}
+	//err = r.Client.Create(context.Background(), ppillDsObj)
+	//if err != nil {
+	//	r.Log.Error(err, "failed to create poison pill daemonset")
+	//	return ctrl.Result{}, err
+	//}
 
 	r.Log.Info("reconciled succesfully")
 	return ctrl.Result{}, nil
@@ -74,4 +107,33 @@ func (r *PoisonPillConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&configpoisonpilliov1alpha1.PoisonPillConfig{}).
 		Complete(r)
+}
+
+func (r *PoisonPillConfigReconciler) parseK8sYaml(fileR []byte) []runtime.Object {
+	acceptedK8sTypes := regexp.MustCompile(`(Namespace|Role|ClusterRole|RoleBinding|ClusterRoleBinding|ServiceAccount|DaemonSet|Service)`)
+	fileAsString := string(fileR[:])
+	sepYamlfiles := strings.Split(fileAsString, "---")
+	retVal := make([]runtime.Object, 0, len(sepYamlfiles))
+	for _, f := range sepYamlfiles {
+		if f == "\n" || f == "" {
+			// ignore empty cases
+			continue
+		}
+
+		decode := scheme.Codecs.UniversalDeserializer().Decode
+		obj, groupVersionKind, err := decode([]byte(f), nil, nil)
+
+		if err != nil {
+			r.Log.Error(err, "error while decoding YAML object")
+			continue
+		}
+
+		if !acceptedK8sTypes.MatchString(groupVersionKind.Kind) {
+			log.Printf("the custom-roles configMap contained object types which are not supported! Skipping object with type: %s", groupVersionKind.Kind)
+		} else {
+			retVal = append(retVal, obj)
+		}
+
+	}
+	return retVal
 }
